652ea319952439c64811fd18749b51bdb52b027b
Abincjoseph20 <ebincjoseph2@gmail.com>
2025-10-13 21:17:02 +0530

    cv updated

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -7,7 +7,7 @@ function Home() {
 
   // Function to handle CV download
   const handleDownloadCV = () => {
-    const cvUrl = '/documents/abin_C_j_Resume (2).pdf';
+    const cvUrl = '/documents/abin_c_j_Resume (4).pdf';
     const link = document.createElement('a');
     link.href = cvUrl;
     link.download = 'Abion_CJ_CV.pdf';
